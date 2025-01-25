9f4f07fb881172af39f02befa63019ec501d5307
aminalundin <lundinamina1@gmail.com>
2025-01-25 20:40:47 +0100

    logic for chat form to display messages from input field

--- public/app.js
+++ public/app.js
@@ -8,6 +8,9 @@ const messageInput = document.querySelector("#message")
 // websocket
 const webSocket = new WebSocket("ws://localhost:8080");
 
+// declare object for chat messages
+let objChat = {};
+
 // event listeners
 
 // user form
@@ -23,24 +26,43 @@ userForm.addEventListener("submit", (e) => {
 
     // active messaging
     messageInput.focus();
+
+    objChat.user = userInput.value;
     
 });
 
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    objChat.message = messageInput.value;
+
+    // clear input field
+    messageInput.value = "";
+
+    // call for function to update chat history
+    renderChat(objChat)
+});
 
 
+/**
+ *
+ *
+ * @param {object} obj
+ */
+function renderChat(obj) {
 
+    const div = document.createElement("div");
+    const p = document.createElement("p")
+    p.textContent = obj.message;
 
+    const span = document.createElement("span");
+    span.textContent = obj.user;
 
+    div.appendChild(p);
+    div.appendChild(span);
 
+    chat.appendChild(div);
 
-// setTimeout(() => {
-    
-    // let msg = {
-    //     type: "chat",
-    //     message: "hello world"
-    // }
+}
 
-    
-//     webSocket.send(JSON.stringify(msg));
-//     console.log("message sent to server")
-// }, 5000);
+renderChat({user: objChat.user, message: objChat.message});
